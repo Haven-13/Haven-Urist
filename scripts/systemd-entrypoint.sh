40d0c056a7d6db4dd5e230a535cb80d9ec9ad0c8
Martin Lyrå <martin.lyra.97@gmail.com>
2020-08-17 23:09:57 +0200

    Update configuration

--- scripts/systemd-entrypoint.sh
+++ scripts/systemd-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+DD_ARGS="-close" # added to the DreamDaemon in docker-entrypoint.sh
+RUN_BRANCH=master
+
 git --version
-git checkout master
-git pull origin master
+git pull origin
+git checkout $RUN_BRANCH
 docker-compose down
 docker-compose up --build --exit-code-from game
