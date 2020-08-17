118b1fcd1afc43510d7449c305f6839e4ebab4a3
Martin Lyrå <martin.lyra.97@gmail.com>
2020-08-18 01:17:54 +0200

    Modify server scripts

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir /byond
-chown $RUN_AS:$RUN_AS /byond $BUILD_DIR baystation12.rsc
+chown $RUN_AS:$RUN_AS /byond $BUILD_DIR $BUILD_TARGET.rsc
 
-gosu $RUN_AS DreamDaemon baystation12.dmb 8000 -trusted -verbose $DD_ARGS
+gosu $RUN_AS ./scripts/server_dd_wrapper.sh
