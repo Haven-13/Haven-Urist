335faa289a52d14cb1f21f5dcbb2af6209dad091
Martin Lyrå <martin.lyra.97@gmail.com>
2020-08-17 21:25:35 +0200

    Update systemd-entrypoint.sh

--- scripts/systemd-entrypoint.sh
+++ scripts/systemd-entrypoint.sh
@@ -3,5 +3,5 @@
 git --version
 git checkout master
 git pull origin master
-docker-compose down .
-docker-compose up --build --exit-code-from game .
+docker-compose down
+docker-compose up --build --exit-code-from game
