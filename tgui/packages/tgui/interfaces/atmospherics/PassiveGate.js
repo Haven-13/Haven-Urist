a38cda5576350b6d02730ca7a73147daa8086922
Martin <martin.lyra.97@gmail.com>
2020-10-10 07:01:38 +0200

    Starter for passive gate

---
+++ tgui/packages/tgui/interfaces/atmospherics/PassiveGate.js
@@ -0,0 +1,13 @@
+import { useBackend } from "tgui/backend";
+import { Button, LabeledList, NumberInput, ProgressBar, Section } from "tgui/components";
+import { Window } from "tgui/layouts";
+
+export const PassiveGate = (props, context) => {
+  return (
+    <Window>
+      <Window.Content>
+
+      </Window.Content>
+    </Window>
+  );
+}
