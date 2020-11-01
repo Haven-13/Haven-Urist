d95434ecb83ca8f2c34db9dc6d078f2bb7759842
Martin <martin.lyra.97@gmail.com>
2020-11-01 18:15:59 +0100

    Here we go again...

--- .travis/install_rust_g.sh
+++ .travis/install_rust_g.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 source dependencies.sh
 
-mkdir -p $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin
-wget -O $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
-chmod +x $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/librust_g.so
-ldd $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/librust_g.so
+mkdir -p $HOME/.byond/bin
+wget -O $HOME/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
+chmod +x $HOME/.byond/bin/librust_g.so
+ldd $HOME/.byond/bin/librust_g.so
