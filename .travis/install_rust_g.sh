1ef587b06284a69f43b0fcc6bd43f58a2d0085d5
Martin <martin.lyra.97@gmail.com>
2020-11-01 17:27:34 +0100

    Alter rust_g installation destination

--- .travis/install_rust_g.sh
+++ .travis/install_rust_g.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 source dependencies.sh
 
-mkdir -p ~/.byond/bin
-wget -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
-chmod +x ~/.byond/bin/librust_g.so
-ldd ~/.byond/bin/librust_g.so
+mkdir -p $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin
+wget -O $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
+chmod +x $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/librust_g.so
+ldd $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/librust_g.so
