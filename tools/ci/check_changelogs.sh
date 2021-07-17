60265454217edb2bdb559b91697b98c74c5ba4b3
Martin Lyrå <martin.lyra.97@gmail.com>
2021-07-17 05:41:50 +0200

    Test

--- tools/ci/check_changelogs.sh
+++ tools/ci/check_changelogs.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 md5sum -c - <<< "2d8811f2f66cf848f8f19e658aa41300 *.changelog/.example.yml"
+pip3 install ruamel.yaml
 python3 tools/changelog/generate_cl.py --dry-run
 python3 tools/changelog/compile_cl.py html/changelog/changelog.html .changelog --dry-run
 
