beececa5a87eb5a4442954619c319546f637f837
Martin Lyrå <martin.lyra.97@gmail.com>
2021-08-24 14:51:20 +0200

    Improve this a bit

--- tools/ci/check_log.sh
+++ tools/ci/check_log.sh
@@ -3,9 +3,13 @@ set -euo pipefail
 
 source tools/ci/check_code.sh
 
+need_cmd grep
+
 run_test "check tests passed" "grep 'All Unit Tests Passed' $1"
 run_test "check no runtimes" "grep 'Caught 0 Runtimes' $1"
 run_test_fail "check no runtimes 2" "grep 'runtime error:' $1"
 run_test_fail "check no scheduler failures" "grep 'Process scheduler caught exception processing' $1"
 run_test_fail "check no warnings" "grep 'WARNING:' $1"
 run_test_fail "check no failures" "grep 'ERROR:' $1"
+
+check_fail
