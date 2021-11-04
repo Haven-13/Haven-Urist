86e7043cb71e2969e8e5013c993a110a6bc3806b
Martin Lyrå <martin.lyra.97@gmail.com>
2021-11-04 23:51:17 +0100

    More version upgrades

--- dependencies.sh
+++ dependencies.sh
@@ -11,12 +11,12 @@ export BYOND_MINOR=1569
 export RUST_G_VERSION=0.5.0
 
 #node version
-export NODE_VERSION=12
-export NODE_VERSION_PRECISE=12.22.4
+export NODE_VERSION=14
+export NODE_VERSION_PRECISE=14.18.1
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.7
+export SPACEMAN_DMM_VERSION=suite-1.7.1
 
 # Python version for mapmerge and other tools
-export PYTHON_VERSION=3.8.3
+export PYTHON_VERSION=3.9.5
 
