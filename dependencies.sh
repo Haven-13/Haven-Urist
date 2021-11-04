919c70f07035b1e61ddbbfb0c1e99c14729787e5
martinlyra <martinlyra@users.noreply.github.com>
2021-11-04 21:50:11 +0100

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -4,15 +4,15 @@
 #Final authority on what's required to fully build the project
 
 # byond version
-export BYOND_MAJOR=513
-export BYOND_MINOR=1536
+export BYOND_MAJOR=514
+export BYOND_MINOR=1569
 
 #rust_g git tag
 export RUST_G_VERSION=0.5.0
 
 #node version
 export NODE_VERSION=12
-export NODE_VERSION_PRECISE=12.20.0
+export NODE_VERSION_PRECISE=12.22.4
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.7
